666e20f2baab7e8c30eb5958c1def3a85dc73d3f
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-02 17:46:10 -0400

    ACT

---
+++ BoletoBus.Viaje.Persistance/Context/BoletoBusContext.cs
@@ -0,0 +1,26 @@
+﻿using BoletoBus.Entidades.Domain.Entities.Empleado;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BoletoBus.Viaje.Persistance.Context
+{
+    public class BoletoBusContext : DbContext
+    {
+
+        public BoletoBusContext(DbContextOptions<BoletoBusContext> options) : base(options)
+        {
+
+        }
+
+        #region"DB Sets de Angel Miguel"
+        public DbSet<Entidades.Domain.Entities.Viaje.Viaje> Viaje { get; set; }
+        
+
+
+        #endregion
+
+
+
+    }
+}
--- BoletoBus.Viaje.Persistance/Repositories/ViajeRepository.cs
+++ BoletoBus.Viaje.Persistance/Repositories/ViajeRepository.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+
+
+using BoletoBus.Entidades.Domain.Interfaces.IViaje;
+using System.Linq.Expressions;
 
 namespace BoletoBus.Viaje.Persistance.Repositories
 {
     public class ViajeRepository : IViajeRepository
     {
+        public void Agregar(Entidades.Domain.Entities.Viaje.Viaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Entidades.Domain.Entities.Viaje.Viaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar(Entidades.Domain.Entities.Viaje.Viaje entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Expression<Func<Entidades.Domain.Entities.Viaje.Viaje, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entidades.Domain.Entities.Viaje.Viaje> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entidades.Domain.Entities.Viaje.Viaje GetEntityBy(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entidades.Domain.Entities.Viaje.Viaje> GetViajesByID(int idViaje)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
