861e81999004605b4b36127f846138c4dadf4ede
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-02 16:23:11 -0400

    a

---
+++ BoletoBus.Entidades.Domain/Entities/Empleado/Empleado.cs
@@ -0,0 +1,9 @@
+﻿using BoletoBus.Common.Data.Base;
+
+namespace BoletoBus.Entidades.Domain.Entities.Empleado
+{
+    public class Empleado : AuditEntity<int>
+    {
+        public override int Id { get; set; }
+    }
+}
