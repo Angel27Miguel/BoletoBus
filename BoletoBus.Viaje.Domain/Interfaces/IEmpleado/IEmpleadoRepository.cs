1e4322cd96a03505388be82cc394a17d4213cbba
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-06 09:54:53 -0400

    A

---
+++ BoletoBus.Viaje.Domain/Interfaces/IEmpleado/IEmpleadoRepository.cs
@@ -0,0 +1,13 @@
+﻿using BoletoBus.Common.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletoBus.Viaje.Domain.Interfaces.IEmpleado
+{
+    public interface IEmpleado : IBaseRepository<Entities.Reserva.ReservaDetalle, int>
+    {
+    }
+}
