4da056ff7db2797e66769a5301211d8c484a1bde
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-09 19:41:42 -0400

    Act

--- BoletoBus.Empleado.Application/Interfaces/IEmpleadoServices.cs
+++ BoletoBus.Empleado.Application/Interfaces/IEmpleadoServices.cs
@@ -1,8 +1,20 @@
 ﻿
 
+using BoletoBus.Empleado.Application.Dtos;
+
 namespace BoletoBus.Empleado.Application.Interfaces
 {
     internal interface IEmpleadoServices
     {
+        List<EmpleadoModel> GetEmpleados();
+
+        void GuardarEmpleado(EmpleadosGuardarModel empleadosGuardar);
+
+        void EliminarEmpleados(EmpleadosEliminarModel empleadosEliminar);
+
+        void EditarEmpleados(EmpleadosEditarModel empleadosEditar);
+
+
+        EmpleadoModel GetEmpleado(int IdEmpleado);
     }
 }
