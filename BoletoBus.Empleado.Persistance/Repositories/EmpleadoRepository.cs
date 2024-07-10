26f7e6652cd7e7f20cf4314d272676ac856d35e2
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-09 22:36:25 -0400

    ACT

--- BoletoBus.Empleado.Persistance/Repositories/EmpleadoRepository.cs
+++ BoletoBus.Empleado.Persistance/Repositories/EmpleadoRepository.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using BoletoBus.Empleado.Domain.Entities;
+using BoletoBus.Empleado.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BoletoBus.Empleado.Persistance.Repositories
 {
-    public class EmpleadoRepository
+    public class EmpleadoRepository : IEmpleadoRepository
     {
+        public void Agregar(Empleados entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Empleados entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Eliminar(Empleados entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Expression<Func<Empleados, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleados> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Empleados> GetEmpleadosByID(int idViaje)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Empleados GetEntityBy(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
