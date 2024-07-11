140dae9930ead0e5575b94d85826fd8874a156e9
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-11 00:09:07 -0400

    ACT

--- BoletoBus.Reserva.Persistance/Context/BoletoBusContext.cs
+++ BoletoBus.Reserva.Persistance/Context/BoletoBusContext.cs
@@ -1,17 +1,15 @@
-﻿
-
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace BoletoBus.Empleado.Persistance.Context
 {
     public class BoletoBusContext : DbContext
+
     {
         public BoletoBusContext(DbContextOptions<BoletoBusContext> options) : base(options)
         {
             
         }
 
-        public DbSet<Domain.Entities.Empleados> Empleado { get; set; }
+        public DbSet<Reserva.Domain.Entities.Reservas> Reserva { get; set; }
     }
 }
---
+++ BoletoBus.Reserva.Persistance/Exceptions/ReservaException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BoletoBus.Reserva.Persistance.Exceptions
+{
+    public class ReservaException : Exception
+    {
+        public ReservaException(string massage) : base(massage) 
+        {
+            
+        }
+    }
+}
