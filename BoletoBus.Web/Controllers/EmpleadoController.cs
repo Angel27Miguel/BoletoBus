ba02b531e90835e37e0e56eed27f7a7ceeeb8878
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-07-28 00:03:26 -0400

    ACT

--- BoletoBus.Web/Controllers/EmpleadoController.cs
+++ BoletoBus.Web/Controllers/EmpleadoController.cs
@@ -1,62 +1,115 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BoletoBus.Web.Models.EmpleadosModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BoletoBus.Web.Controllers
 {
     public class EmpleadoController : Controller
     {
         // GET: EmpleadoController
-        public ActionResult Index()
+        HttpClientHandler httpHandler = new HttpClientHandler();
+
+        public EmpleadoController()
         {
-            return View();
+            this.httpHandler = new HttpClientHandler();
+
+        }
+        public async Task<ActionResult> Index()
+        {
+            EmpleadoGetListResult empleadoGetList = new EmpleadoGetListResult();
+
+            using (var httpClient = new HttpClient(this.httpHandler))
+            {
+                var url = "http://localhost:5297/api/Empleado/GetEmpleado";
+
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+
+                        empleadoGetList = JsonConvert.DeserializeObject<EmpleadoGetListResult>(apiResponse);
+
+                        if (!empleadoGetList.success)
+                        {
+                            ViewBag.Massage = empleadoGetList;
+                            return View();
+                        }
+                    }
+                }
+            }
+            return View(empleadoGetList.date);
         }
 
         // GET: EmpleadoController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
+        { 
+            EmpleadoGetResult empleadoGetResult = new EmpleadoGetResult();
+
+            using (var httpClient = new HttpClient(this.httpHandler))
+            {
+                var url = $"http://localhost:5297/api/Empleado/GetEmpleadoById?id={id}";
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+
+                        empleadoGetResult = JsonConvert.DeserializeObject<EmpleadoGetResult>(apiResponse);
+
+                        if (!empleadoGetResult.success)
+                        {
+                            ViewBag.Massage = empleadoGetResult;
+                            return View();
+                        }
+                    }
+                }
+            }
+            return View(empleadoGetResult.date);
+        }
+
+    
+
+    // GET: EmpleadoController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: EmpleadoController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(EmpleadoGuardarModel empleadoGuardar)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
         {
             return View();
-        }
-
-        // GET: EmpleadoController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: EmpleadoController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: EmpleadoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: EmpleadoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
+
+    //    // GET: EmpleadoController/Edit/5
+    //    public ActionResult Edit(int id)
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: EmpleadoController/Edit/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit(int id, IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
+    //}
 }
---
+++ BoletoBus.Web/Models/EmpleadosModels/EmpleadoGuardarModel.cs
@@ -0,0 +1,6 @@
+﻿namespace BoletoBus.Web.Models.EmpleadosModels
+{
+    public class EmpleadoGuardarModel : EmpleadoModelBase
+    {
+    }
+}
